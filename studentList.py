437235afb892875902b9cc3da126c7b8ce459955
rutu <rutu@localhost>
2026-06-13 18:31:55 +0530

    Improve studentList menu and input handling

--- studentList.py
+++ studentList.py
@@ -1,28 +1,77 @@
-
 def studentList():
     def listStudents():
-        for item in students:
-            index = students.index(item)
-            sr_no = index + 1
-            print(f"{sr_no}. {item}")
+        if not students:
+            print('\n"No students in the list."\n')
+            return
+
+        print('\nStudent list:')
+        for index, student in enumerate(students, start=1):
+            print(f'{index}. {student}')
+        print('-' * 8)
+
+    def searchStudents():
+        search = input('Enter the name of the student to search: ').strip()
+        if not search:
+            print('\nName cannot be empty.\n')
+            return
+
+        name = search.capitalize()
+        if name in students:
+            print(f'\n{name} is in the list.\n')
+        else:
+            print(f'\n{name} is not in the list.\n')
+
+    def addStudents():
+        nameAdd = input('Enter the name of the student: ').strip()
+        if not nameAdd:
+            print('\nName cannot be empty.\n')
+            return
+
+        students.append(nameAdd.capitalize())
+        print(f'\n{nameAdd.capitalize()} has been added to the list.\n')
+
+    def removeStudents():
+        toRemove = input('Enter the name of the student to remove: ').strip()
+        if not toRemove:
+            print('\nName cannot be empty.\n')
+            return
+
+        name = toRemove.capitalize()
+        if name in students:
+            students.remove(name)
+            print(f'\n{name} has been removed from the list.\n')
+        else:
+            print(f'\n{name} is not in the list.\n')
+
     students = []
     try:
         while True:
-            if students == []:
-                nameAdd = input("Enter the name of the student: ")
-                students.append(nameAdd.capitalize())
-                listStudents()
-            elif students != []:
-                nameAdd = input("Enter the name of the student: ")
-                students.append(nameAdd.capitalize())
-                listStudents()
-                remove = input("Do you want to remove any student? (y/n): ")
-                if remove.lower() == "y":
-                    toRemove = input("Enter the name of the student to remove: ")
-                    students.remove(toRemove.capitalize())
-                    listStudents()
-    except KeyboardInterrupt:
-        print("exited!")
+            menu = (
+                '1. List Students\n'
+                '2. Search Students\n'
+                '3. Add Students\n'
+                '4. Remove Students\n'
+                '5. Exit'
+            )
+            print(menu)
+            userInput = input('Choose an option: ').strip()
 
-if __name__ == "__main__":
+            if userInput == '1':
+                listStudents()
+            elif userInput == '2':
+                searchStudents()
+            elif userInput == '3':
+                addStudents()
+            elif userInput == '4':
+                removeStudents()
+            elif userInput == '5':
+                print('Exiting the program.')
+                break
+            else:
+                print('Invalid option. Please try again.')
+    except KeyboardInterrupt:
+        print('\nexited!')
+
+
+if __name__ == '__main__':
     studentList()
